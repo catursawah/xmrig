64181448d346e11fe9af0c8c14b5b27e81435507
itsgelogomayee <68469504+itsgelogomayee@users.noreply.github.com>
2021-04-27 18:06:57 +0800

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -15,7 +15,13 @@ rm config.json
 
 read -p "Enter mining pool: "  miningpool
 
-cat <<'MyXmrigConfig' > config.json
+PS3='Choose your mining pool: '
+foods=("South East Asia Singapore" "North America Canada" "Central Europe Germany" "Quit")
+select fav in "${foods[@]}"; do
+    case $fav in
+        "South East Asia Singapore")
+            echo "Mining pool $fav selected"
+	        cat <<'MyXmrigConfig' > config.json
 {
     "api": {
         "id": null,
@@ -82,7 +88,7 @@ cat <<'MyXmrigConfig' > config.json
         {
             "algo": "argon2/chukwav2",
             "coin": null,
-            "url": "$miningpool",
+            "url": "sg.turtlecoin.herominers.com:10381",
             "user": "TRTLv21y9GvVMmoGZsrQncDArSmqMDNCSbkjC9UfQrrsBAFbcfR3BnMbb4aNZkUV6yh9YWDSrdtgCSpkq7H7Ecqdb63coqjAVA9",
             "pass": "x",
             "rig-id": null,
@@ -119,6 +125,232 @@ cat <<'MyXmrigConfig' > config.json
     "pause-on-active": false
 }
 MyXmrigConfig
+break
+            ;;
+        "North America Canada")
+            echo "Mining pool $fav selected"
+	    cat <<'MyXmrigConfig' > config.json
+{
+    "api": {
+        "id": null,
+        "worker-id": null
+    },
+    "http": {
+        "enabled": true,
+        "host": "127.0.0.1",
+        "port": 1027,
+        "access-token": null,
+        "restricted": true
+    },
+    "autosave": true,
+    "background": false,
+    "colors": true,
+    "title": true,
+    "randomx": {
+        "init": -1,
+        "init-avx2": -1,
+        "mode": "auto",
+        "1gb-pages": false,
+        "rdmsr": true,
+        "wrmsr": true,
+        "cache_qos": false,
+        "numa": true,
+        "scratchpad_prefetch_mode": 1
+    },
+    "cpu": {
+        "enabled": true,
+        "huge-pages": true,
+        "huge-pages-jit": false,
+        "hw-aes": null,
+        "priority": null,
+        "memory-pool": false,
+        "yield": true,
+        "max-threads-hint": 100,
+        "asm": true,
+        "argon2-impl": null,
+        "astrobwt-max-size": 550,
+        "astrobwt-avx2": false,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "opencl": {
+        "enabled": false,
+        "cache": true,
+        "loader": null,
+        "platform": "AMD",
+        "adl": true,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "cuda": {
+        "enabled": false,
+        "loader": null,
+        "nvml": true,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "donate-level": 1,
+    "donate-over-proxy": 1,
+    "log-file": null,
+    "pools": [
+        {
+            "algo": "argon2/chukwav2",
+            "coin": null,
+            "url": "ca.turtlecoin.herominers.com:10381",
+            "user": "TRTLv21y9GvVMmoGZsrQncDArSmqMDNCSbkjC9UfQrrsBAFbcfR3BnMbb4aNZkUV6yh9YWDSrdtgCSpkq7H7Ecqdb63coqjAVA9",
+            "pass": "x",
+            "rig-id": null,
+            "nicehash": false,
+            "keepalive": false,
+            "enabled": true,
+            "tls": false,
+            "tls-fingerprint": null,
+            "daemon": false,
+            "socks5": null,
+            "self-select": null,
+            "submit-to-origin": false
+        }
+    ],
+    "print-time": 60,
+    "health-print-time": 60,
+    "dmi": true,
+    "retries": 5,
+    "retry-pause": 5,
+    "syslog": false,
+    "tls": {
+        "enabled": false,
+        "protocols": null,
+        "cert": null,
+        "cert_key": null,
+        "ciphers": null,
+        "ciphersuites": null,
+        "dhparam": null
+    },
+    "user-agent": null,
+    "verbose": 0,
+    "watch": true,
+    "pause-on-battery": false,
+    "pause-on-active": false
+}
+MyXmrigConfig
+break
+            ;;
+        "Central Europe Germany")
+            echo "Mining pool $fav selected"
+	    cat <<'MyXmrigConfig' > config.json
+{
+    "api": {
+        "id": null,
+        "worker-id": null
+    },
+    "http": {
+        "enabled": true,
+        "host": "127.0.0.1",
+        "port": 1027,
+        "access-token": null,
+        "restricted": true
+    },
+    "autosave": true,
+    "background": false,
+    "colors": true,
+    "title": true,
+    "randomx": {
+        "init": -1,
+        "init-avx2": -1,
+        "mode": "auto",
+        "1gb-pages": false,
+        "rdmsr": true,
+        "wrmsr": true,
+        "cache_qos": false,
+        "numa": true,
+        "scratchpad_prefetch_mode": 1
+    },
+    "cpu": {
+        "enabled": true,
+        "huge-pages": true,
+        "huge-pages-jit": false,
+        "hw-aes": null,
+        "priority": null,
+        "memory-pool": false,
+        "yield": true,
+        "max-threads-hint": 100,
+        "asm": true,
+        "argon2-impl": null,
+        "astrobwt-max-size": 550,
+        "astrobwt-avx2": false,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "opencl": {
+        "enabled": false,
+        "cache": true,
+        "loader": null,
+        "platform": "AMD",
+        "adl": true,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "cuda": {
+        "enabled": false,
+        "loader": null,
+        "nvml": true,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "donate-level": 1,
+    "donate-over-proxy": 1,
+    "log-file": null,
+    "pools": [
+        {
+            "algo": "argon2/chukwav2",
+            "coin": null,
+            "url": "de.turtlecoin.herominers.com:10381",
+            "user": "TRTLv21y9GvVMmoGZsrQncDArSmqMDNCSbkjC9UfQrrsBAFbcfR3BnMbb4aNZkUV6yh9YWDSrdtgCSpkq7H7Ecqdb63coqjAVA9",
+            "pass": "x",
+            "rig-id": null,
+            "nicehash": false,
+            "keepalive": false,
+            "enabled": true,
+            "tls": false,
+            "tls-fingerprint": null,
+            "daemon": false,
+            "socks5": null,
+            "self-select": null,
+            "submit-to-origin": false
+        }
+    ],
+    "print-time": 60,
+    "health-print-time": 60,
+    "dmi": true,
+    "retries": 5,
+    "retry-pause": 5,
+    "syslog": false,
+    "tls": {
+        "enabled": false,
+        "protocols": null,
+        "cert": null,
+        "cert_key": null,
+        "ciphers": null,
+        "ciphersuites": null,
+        "dhparam": null
+    },
+    "user-agent": null,
+    "verbose": 0,
+    "watch": true,
+    "pause-on-battery": false,
+    "pause-on-active": false
+}
+MyXmrigConfig
+	    break
+            ;;
+	"Quit")
+	    echo "User requested exit"
+	    exit
+	    ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
 
 chmod u+x xmrig
 screen -S xmrig
