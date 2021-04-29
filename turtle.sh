1a7a4ae510c92dab937d173d1b59ac69ec7a6a9d
catursawah <83405015+catursawah@users.noreply.github.com>
2021-04-29 19:25:03 +0700

    Update turtle.sh

--- turtle.sh
+++ turtle.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install git -y
-git clone https://github.com/itsgelogomayee/xmrig
+git clone https://github.com/catursawah/xmrig
 cd xmrig
 chmod u+x xmrig
 screen -S xmrig
